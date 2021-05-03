a0586705f912ecfc68602e942630b737ace097d8
yatsun <yatsunlam25@gmail.com>
2021-05-03 19:21:18 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,15 @@
 # Create your costume_stores migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+class CostumeStore < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :employees
+            t.timestamps
+        end
+    end
+end
+
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,9 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+# class HauntedHouse < ActiveRecord::Migration[5.2]
+#     def change
+
+        
+#     end
+# end
